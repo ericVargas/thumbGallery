f1aaeb819eb98b82093f62cea1189140a97d5108
ericVargas <batman@erics-mbp-2.home>
2018-06-04 05:14:45 -0400

    added fade in animation for main img

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
-const opacity = 0.4;
+const opacity = 0.6;
 
 //// ES6
 //const [current, imgs] = [
 //    document.querySelector('#current'), document.querySelectorAll('.imgs img')
 //];
+
+// Set first img opacity
+imgs[0].style.opacity = opacity;
 
 imgs.forEach(img => img.addEventListener('click', imgClick));
 
@@ -15,6 +18,12 @@ function imgClick(e) {
     
     // Change current image to src of clicked image
     current.src = e.target.src;
+    
+    // Add fade-in class
+    current.classList.add('fade-in');
+    
+    // Remove fade-in class after .5 seconds
+    setTimeout(() => current.classList.remove('fade-in'), 500);
     
     // Change the opacity to opacity var
     e.target.style.opacity = opacity;
