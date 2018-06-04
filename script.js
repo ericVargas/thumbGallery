fd09d386dd7ff573899f4ab642600edec0c48fef
ericVargas <batman@erics-mbp-2.home>
2018-06-04 04:57:09 -0400

    placed opacity on selected image

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.4;
 
 //// ES6
 //const [current, imgs] = [
 //    document.querySelector('#current'), document.querySelectorAll('.imgs img')
 //];
 
-imgs.forEach(img => img.addEventListener('click', (e) => (current.src = e.target.src))
-);
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+    // Reset the opacity
+    imgs.forEach(img => (img.style.opacity = 1));
+    
+    // Change current image to src of clicked image
+    current.src = e.target.src;
+    
+    // Change the opacity to opacity var
+    e.target.style.opacity = opacity;
+}
